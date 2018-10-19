e32b0a797b69ed5a0ee77b36c0d2cc688f82cdbe
Richard Scott <git@pointb.co.uk>
2018-10-19 11:36:02 +0100

    Updated installer flags for curl to be silent

--- bin/install.sh
+++ bin/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 GIT_REPO="https://raw.githubusercontent.com/dircmd/dircmd/master"
-curl --output /etc/profile.d/dircmd.sh ${GIT_REPO}/src/bash
+curl --silent --output /etc/profile.d/dircmd.sh ${GIT_REPO}/src/bash
