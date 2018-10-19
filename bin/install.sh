8066e2f55cf5ba10e074e63c93300966d8f2de1e
Richard Scott <5149834+richard-scott@users.noreply.github.com>
2018-10-19 11:42:56 +0100

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 GIT_REPO="https://github.com/dircmd/dircmd/raw/master"
 curl --location --silent --output /etc/profile.d/dircmd.sh ${GIT_REPO}/src/bash
+sleep 2s && source /etc/profile.d/dircmd.sh
